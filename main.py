db6ac84a8f3d5fb6f93f4bb9cfcc067ef01d450c
isoqovrozimurod <77664688+isoqovrozimurod@users.noreply.github.com>
2025-08-27 19:04:08 +0500

    Update main.py
    kredit.py fayli ro'yxatdan o'tkazildi

--- main.py
+++ main.py
@@ -16,12 +16,14 @@ from ish_haqi import router as ish_haqi_router
 from garov import router as garov_router
 from calculator import router as calc_router
 from buyruqlar import set_bot_commands
+from kredit import router as kredit_admin_router  # Admin uchun
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
+
 async def main():
     if not TOKEN:
         logger.error("❌ BOT_TOKEN .env fayldan topilmadi.")
@@ -32,15 +34,15 @@ async def main():
 
     dp = Dispatcher(storage=MemoryStorage())
 
-
     # Routerlarni ro‘yxatga olish
     dp.include_router(start_router)
     dp.include_router(contact_router)
-    dp.include_router(kredit_router)
+    dp.include_router(kredit_router)          # kredit_turlari.py
     dp.include_router(pensiya_router)
     dp.include_router(ish_haqi_router)
     dp.include_router(garov_router)
     dp.include_router(calc_router)
+    dp.include_router(kredit_admin_router)    # kredit.py (faqat admin uchun)
 
     await set_bot_commands(bot)
 

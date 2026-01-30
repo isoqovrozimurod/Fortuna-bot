fc9fcb42dfbe0fa41968a0d0447015a966c39cd5
isoqovrozimurod <77664688+isoqovrozimurod@users.noreply.github.com>
2026-01-30 19:38:58 +0500

    Comment out filiallar_router import
    Comment out the import statement for filiallar_router.

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from control import router as control_router
 from chanel import router as chanel_router
 from chanel import SubscriptionMiddleware
 from kredit import router as kredit_admin_router
-from filiallar import router as filiallar_router
+#from filiallar import router as filiallar_router
 from keep_alive import keep_alive
 
 logging.basicConfig(

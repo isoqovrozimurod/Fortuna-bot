88c49e402073a11a9063f381688033f01b2acc4c
isoqovrozimurod <77664688+isoqovrozimurod@users.noreply.github.com>
2026-09-10 15:22:44 +0500

    Update pensiya.py

--- pensiya.py
+++ pensiya.py
@@ -22,7 +22,7 @@ async def show_pensiya_info(callback: CallbackQuery, bot: Bot):
         "  • Pasport\n"
         "  • Pensiya plastik kartasi\n\n"
         
-        "📞 <b>Murojaat uchun:</b>\n"
+        "<b>Murojaat uchun:</b>\n"
         "📱 +998 99 251 00 40\n"
         "📱 +998 95 375 45 40\n\n"
         
